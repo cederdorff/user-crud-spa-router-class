88e6102acc816df42c54fc3b7e339e5158f80f6a
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-29 20:39:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
+// ========== imports ========== //
 import "./router.js";
-console.log("app.js is running!");
+import userService from "./user-service.js";
+
+function displayUsers(userList) {
+    document.querySelector("#users-grid").innerHTML = "";
+
+    for (const user of userList) {
+        const html = /*html*/ `
+             <article>
+                <img src="${user.image}">
+                <h2>${user.name}</h2>
+                <a href="mailto:${user.mail}">${user.mail}</a>
+                 <div>
+                    <button class="btn-update-user" data-id="${user.id}">Update</button>
+                    <button class="btn-delete-user" data-id="${user.id}">Delete</button>
+                </div>
+            </article>
+        `;
+        document.querySelector("#users-grid").insertAdjacentHTML("afterbegin", html);
+    }
+
+    document.querySelector("#users-grid").innerHTML = html;
+}
+
+async function onUsersListChanged() {
+    const users = await userService.getUsers();
+    displayUsers(users);
+}
+
+onUsersListChanged();
